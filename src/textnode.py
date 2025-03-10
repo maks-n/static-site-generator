e09437455638fa6c10ac82f1754e04bee53ee493
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-09 21:17:02 -0400

    upd textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = "normal"
-    BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
-    LINK_TEXT = "link"
-    IMAGE_TEXT = "image"
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -14,9 +14,10 @@ class TextNode:
         self.text_type = text_type
         self.url = url
 
-    def __eg__(self, other):
-        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type.value == other.text_type.value and self.url == other.url:
             return True
+        return False
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
