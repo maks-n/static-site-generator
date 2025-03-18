6479313808842ecb31b2ce73981b552ec86320bb
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-18 17:23:05 -0400

    add __repr__ to LeafNode. update logic of to_html in LeafNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -8,7 +8,8 @@ class LeafNode(HTMLNode):
         if self.value is None:
             raise ValueError("Value is required for all LeafNodes")
         if self.tag is None:
-            return str(self.value)
-        if self.props:
-            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"            
-        return f"<{self.tag}>{self.value}</{self.tag}>"        
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
