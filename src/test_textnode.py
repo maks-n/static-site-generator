ee97ba1cc733746a905302c63370ff50ef9f3bb5
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-17 20:07:41 -0400

    add text_node_to_html_node transformation and test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -15,7 +15,7 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
     def test_not_eq(self):
-        node = TextNode("Hello", TextType.NORMAL)
+        node = TextNode("Hello", TextType.TEXT)
         node2 = TextNode("World", TextType.CODE)
         self.assertNotEqual(node, node2)
 
@@ -23,6 +23,33 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Hello", TextType.LINK, "test.com")
         node2 = TextNode("Hello", TextType.LINK, "wrong.net")
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+    
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.props, {"href":"google.com"})
+    
+    def test_img(self):
+        node = TextNode("", TextType.IMAGE, "google.com", "alt text")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src":"google.com", "alt":"alt text"})
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -9,15 +10,39 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text, text_type, url=None, alt=None):
         self.text = text
         self.text_type = text_type
         self.url = url
+        self.alt = alt
 
     def __eq__(self, other):
-        if self.text == other.text and self.text_type.value == other.text_type.value and self.url == other.url:
+        if self.text == other.text and self.text_type.value == other.text_type.value and self.url == other.url and self.alt == other.alt:
             return True
         return False
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node is None:
+        raise Exception("TextNode is None")
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case TextType.IMAGE:
+            if text_node.url is None:
+                raise Exception(f"TextNode IMAGE missing URL, text: {text_node.text}")
+            if text_node.alt is None:
+                raise Exception(f"TextNode IMAGE missing alt, text: {text_node.text}")
+            return LeafNode("img", "", {"src":text_node.url, "alt":text_node.alt})
+        case _:
+            raise Exception("Unknown TextType of the TextNode")
