ec331475b032f5ec2263899aecc3b96c1e8cf094
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-19 19:14:35 -0400

    refactor ParentNode and LeafNode into HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,36 @@ class HTMLNode():
         for k,v in self.props.items():
             result += f" {k}=\"{v}\""
         return result
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value is required for all LeafNodes")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag is required for all ParentNodes")
+        if self.children is None:
+            raise ValueError("Children is required for all ParentNodes")
+        result = ""
+        for child in self.children:
+            result += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{result}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -19,6 +19,59 @@ class TestHTMLNode(unittest.TestCase):
             result == ' href="https://www.google.com" target="_blank"' or
             result == ' target="_blank" href="https://www.google.com"'
         )
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_leaf_to_html_a_2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com" target="_blank">Click me!</a>')
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("a", None)
+        with self.assertRaises(ValueError) as context:
+            node.to_html()
+        self.assertEqual(str(context.exception), "Value is required for all LeafNodes")
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+    
+    def test_to_html_with_grandchildren_2(self):
+        child_node_1 = LeafNode("b", "Bold text")
+        child_node_2 = LeafNode(None, "Normal text")
+        child_node_3 = LeafNode("i", "italic text")
+        child_node_4 = LeafNode(None, "Normal text")
+        parent_node = ParentNode("p", [child_node_1, child_node_2, child_node_3, child_node_4])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
