895d1403317dbdca264babeb7811abef9a73952f
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-20 18:26:14 -0400

    refactor copy_files as independent file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-from textnode import TextNode, TextType
+from copy_files import copy_files_recursive
 
 dir_path_public = "./public"
 dir_path_static = "./static"
@@ -14,18 +14,5 @@ def main():
     print("Copying files from 'static' to 'public' directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-def copy_files_recursive(source_dir_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
-    
-    for filename in os.listdir(source_dir_path):
-        from_path = os.path.join(source_dir_path, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        print(f"*** {from_path} -> {dest_path}")
-        if os.path.isfile(from_path):
-            shutil.copy(from_path, dest_path)
-        else:
-            copy_files_recursive(from_path, dest_path)
-    
-    
+
 main()
