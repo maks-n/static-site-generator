074320c9cf77925f6ef1372280fa24b905db77c5
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-23 20:51:19 -0400

    update generator to reflect basepath

--- src/generator.py
+++ src/generator.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
     raise ValueError("Invalid markdown: no title found, title must start with '# '")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, "r") as from_file, open(template_path, "r") as template_file:
@@ -26,6 +26,8 @@ def generate_page(from_path, template_path, dest_path):
         title = extract_title(markdown_content)
         template = template.replace("{{ Title }}", title)
         template = template.replace("{{ Content }}", html)
+        template = template.replace('href="/"', f'href="{basepath}"')
+        template = template.replace('src="/"', f'src="{basepath}"')
 
         dest_dir_path = os.path.dirname(dest_path)
         if dest_dir_path != "":
@@ -34,12 +36,13 @@ def generate_page(from_path, template_path, dest_path):
         with open(dest_path, "w") as dest_file:
             dest_file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
