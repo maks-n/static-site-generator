defc051a73c0509c163743949e35bf1630e33f38
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-23 20:17:46 -0400

    add generate_pages_recursive to recursively generate all pages

--- src/generator.py
+++ src/generator.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from markdown_blocks import markdown_to_html_node
 
@@ -32,3 +33,13 @@ def generate_page(from_path, template_path, dest_path):
         
         with open(dest_path, "w") as dest_file:
             dest_file.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
