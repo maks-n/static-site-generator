3e8cc63d12ac2df08e512b53e59817c4bab6dc82
maks-n <71183975+maks-n@users.noreply.github.com>
2025-03-23 20:52:49 -0400

    update src/main.py to work with CLI argument and ./docs directory

--- src/main.py
+++ src/main.py
@@ -1,25 +1,31 @@
 import os
 import shutil
+import sys
 
 from copy_files import copy_files_recursive
 from generator import generate_pages_recursive
 
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_static = "./static"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    print("Deleting 'public' directory...")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
+    print(f"Deleting {dir_path_public} directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     
-    print("Copying files from 'static' to 'public' directory...")
+    print(f"Copying files from {dir_path_static} to {dir_path_public} directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating pages...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
